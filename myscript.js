b94e1a195bc27e7c11170419e65b793ac50203e4
sarahelshark <sarahelsh183@gmail.com>
2023-12-14 00:37:07 +0100

    feat add
    new event listener to get my user input

--- myscript.js
+++ myscript.js
@@ -5,11 +5,17 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 *TO DO*
 
--Chiedere all’utente di inserire una parola,per poi estrearne il valore e salvarlo in variabile
-- Creare una funzione per capire se la parola inserita è palindroma, che entra in gioco al click del form.
+-Chiedere all’utente di inserire una parola,per poi estrearne il valore e salvarlo in variabile (event listener alla fine, usa prompt per niziare)
+- Creare una funzione per capire se la parola inserita è palindroma, che entra in gioco al click del bottone
 -Stampare un messagio contenente il risultato per l'utente, ad ogni tentativo
 */
+const btn = document.getElementById("checkBtn");
+let parola = document.getElementById("wordInput").value;
 
-console.log('hello')
-
+btn.addEventListener("click", 
+    function() {
+        let parola = document.getElementById("wordInput").value;
+        console.log(parola);
+    }
+);
 
