f1917b10a4fa631de98238a571bfca5e81e13d37
sarahelshark <sarahelsh183@gmail.com>
2023-12-14 00:11:13 +0100

    first commit
    add some html content+js

---
+++ myscript.js
@@ -0,0 +1,15 @@
+/*Palidroma
+
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+
+*TO DO*
+
+-Chiedere all’utente di inserire una parola,per poi estrearne il valore e salvarlo in variabile
+- Creare una funzione per capire se la parola inserita è palindroma, che entra in gioco al click del form.
+-Stampare un messagio contenente il risultato per l'utente, ad ogni tentativo
+*/
+
+console.log('hello')
+
+
