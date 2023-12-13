13c69c8a54c253f649be4556c50a2a8ab6152850
sarahelshark <sarahelsh183@gmail.com>
2023-12-14 00:46:23 +0100

    feat add
    function

--- myscript.js
+++ myscript.js
@@ -17,6 +17,31 @@ let parola = document.getElementById("wordInput").value;
 btn.addEventListener("click", 
     function() {
         let parola = document.getElementById("wordInput").value;
-        console.log(parola);
+        //console.log(parola);
     }
 );
+
+
+// dichiarare la funzione per controllare il palindromo, la funzione mi deve ciclare al contrario le parole inserite e dovra avere delle condizioni, quindi serve un ciclo for diverso dal solito e i condizionali con i booleani
+
+// creo la booleana di controllo 
+let check = false;
+
+function reverseWord(parola) {
+    // invertire la parola per confrontarla
+    //let reversed = 'it works';
+    let reversed = '';
+    for ( let i = parola.length -1; i >= 0; i--) {
+        reversed += parola[i];
+    }
+    //console.log(reversed);
+    // se le parole coincidono la parola è palindroma
+    // stampare se la parola è palindoma o meno 
+    if (parola == reversed) {
+        // cambio la booleana
+        check = true;
+        //console.log('it works')
+    } else {
+        check = false;
+    }
+}
